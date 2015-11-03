65e2a6d886ff12aaee92a73b31f95aea0de54baf
Craig Bilner <craig.bilner@gmail.com>
2015-11-03 21:59:30 +0000

    Add state change

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const App = () => (
-  <div style={{color: 'red'}}>
-    Hello world!
-  </div>
-);
+export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colour: 'red'
+    };
+  }
+
+  changeMyColour() {
+    console.log('changing colour');
+    this.setState({
+      colour: `#${(~~(Math.random() * (1 << 24))).toString(16)}`
+    });
+  }
+
+  render() {
+    return (
+      <div style={{color: this.state.colour}}>
+        <span>Hello world!</span>
+        <button onClick={this.changeMyColour.bind(this)}>change my colour</button>
+      </div>
+    );
+  }
+}
