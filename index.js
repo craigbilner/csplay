475884a715b1868b634f74c770b45fbe427533f4
Craig Bilner <craig.bilner@gmail.com>
2015-11-03 21:23:06 +0000

    Refactor views to provide index for various options

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const koa = require('koa');
+const router = require('koa-router')();
 const app = koa();
 const { createElement } = require('react');
 const { renderToString } = require('react-dom/server');
 const { App } = require('./src/components/app/app');
 const hbs = require('koa-hbs');
 const common = require('koa-common');
+const HW_VIEW = 'hello-world';
+const html = renderToString(createElement(App));
+const HW_OPTIONS = {
+  mainPlaceholder: html
+};
+
+app.use(common.static('./dist'));
+
+app.use(common.favicon(__dirname + '/public/favicon.ico'));
+
+app.use(hbs.middleware({
+  viewPath: './src/views'
+}));
 
 const csp = [
   'default-src \'none\'',
@@ -14,23 +28,21 @@ const csp = [
   'style-src \'self\''
 ];
 
-app.use(function *(next) {
+router.get('/', function *() {
+  yield this.render('app');
+});
+
+router.get('/nuclear', function *() {
   this.set('Content-Security-Policy', csp.join(';'));
-  yield next;
+
+  yield this.render(HW_VIEW, HW_OPTIONS);
 });
 
-app.use(common.static('./dist'));
-
-app.use(hbs.middleware({
-  viewPath: './src/views'
-}));
-
-app.use(function *() {
-  const html = renderToString(createElement(App));
-  yield this.render('app', {
-    mainPlaceholder: html
-  });
+router.get('/evil', function *() {
+  yield this.render(HW_VIEW, HW_OPTIONS);
 });
+
+app.use(router.routes());
 
 app.listen(3000, () => {
   console.log('listening on 3000');
